3c3f4eedc49343d5a4ad34855fea3abedfd0cbcc
adrichey <1534554+adrichey@users.noreply.github.com>
2025-09-22 17:51:42 -0500

    Introduce SDL2 bindings package; add initial input processing logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adrichey/go-chip8
 
 go 1.24.4
+
+require github.com/veandco/go-sdl2 v0.4.40
--- platform/platform.go
+++ platform/platform.go
@@ -1,6 +1,10 @@
 package platform
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
 
 const VIDEO_HEIGHT = 32
 const VIDEO_WIDTH = 64
@@ -27,4 +31,61 @@ func (s *Screen) Draw() {
 type Platform struct {
 	Keypad [16]byte
 	Screen Screen
+}
+
+func (p *Platform) ProcessInput() bool {
+	quit := false
+
+	for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+		switch t := ebent.(type) {
+		case *sdl.QuitEvent:
+			quit = true
+		case sdl.KeyboardEvent:
+			var s byte = 0
+			if t.Type == sdl.KEYDOWN {
+				s = 1
+			}
+
+			switch t.Keysym.Sym {
+			case sdl.K_ESCAPE:
+				if s == 1 {
+					quit = true
+				}
+			case sdl.K_x:
+				p.Keypad[0] = s
+			case sdl.K_1:
+				p.Keypad[1] = s
+			case sdl.K_2:
+				p.Keypad[2] = s
+			case sdl.K_3:
+				p.Keypad[3] = s
+			case sdl.K_q:
+				p.Keypad[4] = s
+			case sdl.K_w:
+				p.Keypad[5] = s
+			case sdl.K_e:
+				p.Keypad[6] = s
+			case sdl.K_a:
+				p.Keypad[7] = s
+			case sdl.K_s:
+				p.Keypad[8] = s
+			case sdl.K_d:
+				p.Keypad[9] = s
+			case sdl.K_z:
+				p.Keypad[0xA] = s
+			case sdl.K_c:
+				p.Keypad[0xB] = s
+			case sdl.K_4:
+				p.Keypad[0xC] = s
+			case sdl.K_r:
+				p.Keypad[0xD] = s
+			case sdl.K_f:
+				p.Keypad[0xE] = s
+			case sdl.K_v:
+				p.Keypad[0xF] = s
+			}
+		}
+	}
+
+	return quit
 }
